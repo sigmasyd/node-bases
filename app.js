d0015d666ad701dbd0270f12bf4d5d3bbf874acf
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-25 01:36:55 -0500

    Se corrije module.export por module.exports. Se corrige el import de argv usando desestructuracion de em6
    	nuevo archivo:  .gitignore
    	modificado:     app.js
    	nuevo archivo:  config/yargs.js

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 //const fs = require('fs');         // propios o preinstalados
 //const fs = require('express');  // paquetes externos instalados
 //const fs = require('./fs');     // archivos que hemos creado dentro del proyecto
-const argv = require('yargs')
-  .command('listar','Imprime en consola la tabla de multiplicar',{
-    base: {
-      demand: true,
-      alias: 'b'
-    },
-    limite: {
-      alias: 'l',
-      default: 10
-    }
-  })
-  .command('crear','Genera un archivo con la tabla de multiplicar',{
-    base: {
-      demand: true,
-      alias: 'b'
-    },
-    limite: {
-      alias: 'l',
-      default: 10
-    }
-  })
-  .help()
-  .argv;
 
+const { argv } = require('./config/yargs');
 const { crearArchivo , listarTabla } = require('./multiplicar/multiplicar');
+
 let comando = argv._[0];
 console.log(argv);
 switch(comando){
